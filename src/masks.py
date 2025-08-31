ed5cfd7c04775a6fb9129a353bd18872a7e6fe84
Кудряшов Николай <nnikolay2909@gmail.com>
2025-08-31 14:47:55 +0300

    second fixed tests for file transaction_reader.py

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,24 @@
 from typing import Union
+import logging
 
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('../logs/masks.log', mode='w', encoding ="utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+# Реализуйте запись логов в файл. Логи должны записываться в папку
+# logs в корне проекта. Файлы логов должны иметь расширение .log.
+
+# Формат записи лога в файл должен включать
+    # метку времени,
+    # название модуля,
+    # уровень серьезности и
+    # сообщение, описывающее событие или ошибку, которые произошли.
+
+# Лог должен перезаписываться при каждом запуске приложения.
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """
@@ -12,6 +31,7 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
         Маску номера карты, где видны первые 6 цифр и последние 4 цифры остальные символы отображаются *.
         Номер разбит по блокам по 4 цифры, разделенным пробелами.
     """
+    logger.info('Перевод номера карты в строку')
     card_number = str(card_number)
 
     first_index = 0
@@ -19,6 +39,7 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
     number_with_spaces = ""
 
     if card_number.isdigit():
+        logger.info('Проверка корректной длины номера карты')
         if len(card_number) == 16:
 
             for i in range(4):
@@ -26,12 +47,14 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
                 first_index += 4
                 last_index += 4
 
+            logger.info('Маскировка номера карты')
             number_with_spaces_and_asterisks = (
                 number_with_spaces[:7] + "** ****" + number_with_spaces[14:-1]
             )
 
             return number_with_spaces_and_asterisks
 
+    logger.warning('Длина номера карты неправильна')
     return "Длина номера карты неправильна"
 
 
@@ -45,14 +68,22 @@ def get_mask_account(account_number: Union[str, int]) -> str:
     Returns:
         Маску номера счета, где видны только последние 4 цифры номера, а перед ними — две *.
     """
+    logger.info('Перевод номера счета в строку')
     account_number = str(account_number)
 
     if account_number.isdigit():
+        logger.info('Проверка корректной длины номера счета')
         if len(account_number) == 20:
 
+            logger.info('Маскировка номера карты')
             last_four_characters = account_number[-4:]
             last_four_characters_with_asterisks = "**" + last_four_characters
 
             return last_four_characters_with_asterisks
 
-    return "Длина номера карты неправильна"
+    logger.warning('Длина номера счета неправильна')
+    return "Длина номера счета неправильна"
+
+
+get_mask_card_number("7158300734726758")
+get_mask_account("64686473678894779589")
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('../logs/utils.log', mode='w', encoding ="utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def open_file(file_: str) -> list[dict]:
@@ -12,17 +20,23 @@ def open_file(file_: str) -> list[dict]:
         Возвращает список словарей data из json файла operations.
     """
     try:
+        logger.info('Открытие json файла operations')
         with open(file_, "r", encoding="utf-8") as f:
             data = json.load(f)
+            logger.info('Успешное преобразование данных')
             return data
 
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as js_:
+        logger.error(f'Ошибка {js_}')
         return []
-    except FileNotFoundError:
+    except FileNotFoundError as fnf:
+        logger.error(f'Ошибка {fnf}')
         return []
-    except TypeError:
+    except TypeError as te:
+        logger.error(f'Ошибка {te}')
         return []
-    except ValueError:
+    except ValueError as ve:
+        logger.error(f'Ошибка {ve}')
         return []
 
 
